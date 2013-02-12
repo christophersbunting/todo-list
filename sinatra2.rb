192cc46d936f044f35c092de8fa9e30da283e856
Christopher Bunting <christophersbunting@gmail.com>
2013-02-12 01:21:03 -0800

    Database updating

--- sinatra2.rb
+++ sinatra2.rb
@@ -3,12 +3,12 @@ require 'data_mapper'
 require 'sinatra/redirect_with_flash'  
 require 'rubygems'
 require 'dm-timestamps'
-require 'dm-postgres-adapter'  
+require 'dm-postgres-adapter'
 
 SITE_TITLE = "Recall"
 SITE_DESCRIPTION = "'cause you're too busy to remember"
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/recall.db")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/recall.db')
 class Note  
   include DataMapper::Resource  
   property :id, Serial  
